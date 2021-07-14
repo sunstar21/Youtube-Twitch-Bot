d41dfb23c59006a1f1dc07164b0103146361d84a
sunstar21 <81821409+sunstar21@users.noreply.github.com>
2021-07-14 11:42:18 -0400

    Update StartProgram.js

--- StartProgram.js
+++ StartProgram.js
@@ -1,22 +1,125 @@
-var ClientId = "xxvz2mqd0a3dz1v6kxyr9et4dueukh";
-var ClientSecret = "dz04478gkk40xmzerpnm02c9zjd9m5";
+var fs = require('fs');
+var readline = require('readline');
+var {google} = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var clientId;
+var clientSecret;
+var redirectUrl;
+var oauth2Client;
 const http = require('https');
-const axios = require('axios')
-var Youtube = require('youtube-video-api')
+const axios = require('axios');
 const { DownloaderHelper } = require('node-downloader-helper');
-var url = (`https://id.twitch.tv/oauth2/token?grant_type=client_credentials&client_id=${ClientId}&client_secret=${ClientSecret}`);
+var SCOPES = ['https://www.googleapis.com/auth/youtube.upload','https://www.googleapis.com/auth/youtube.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the YouTube API.
+  authorize(JSON.parse(content));
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials) {
+  console.log(credentials);
+  clientSecret = credentials.web.client_secret;
+  clientId = credentials.web.client_id;
+  console.log(clientId);
+  console.log(clientSecret);
+  redirectUrl = credentials.web.redirect_uris[0];
+  oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      console.log("Successful")
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) throw err;
+    console.log('Token stored to ' + TOKEN_PATH);
+  });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+var url = (`https://id.twitch.tv/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`);
+console.log(url);
 axios
   .post(url)
   .then(res => {
     var access_token = res.data.access_token;
-    console.log(access_token)
     var options = {
         host: "api.twitch.tv",
         port: 443,
         path: "/helix/games/top",
         method: "GET",
         headers: {
-            "Client-Id": ClientId,
+            "Client-Id": clientId,
             "Authorization": `Bearer ${access_token}` 
         }
     }
@@ -42,7 +145,7 @@ axios
                     path: `/helix/clips?game_id=${id}`,
                     method: "GET",
                     headers: {
-                        "Client-Id": ClientId,
+                        "Client-Id": clientId,
                         "Authorization": `Bearer ${access_token}` 
                     }
                   }
@@ -56,40 +159,44 @@ axios
                         var d = st.data;
                         for(var i = 0; i < d.length; i++) {
                             var dp = d[i].thumbnail_url.split("-preview")[0];
+                            console.log(dp);
+                            var downloadname = dp.split("https://clips-media-assets2.twitch.tv/")[1];
                             dp+=".mp4"
                             var nm3 = data[i].name;
-                            //onst dl = new DownloaderHelper(dp , "/Users/rahilshariff/Downloads");
-                            //dl.on('end', () => console.log('Download Completed'))
-                            //dl.start(); 
-                            
-                            var youtube = Youtube({ 
-                                video: {
-                                    part: 'status,snippet' 
-                                }
-                            })
-                            var params = {
-                                resource: {
-                                snippet: {
-                                    title: nm3,
-                                    description: `Thank you for watching! Check out ${d[i].creator_name} on twitch for more videos by him! Like and Subscribe for more!`
+                            console.log("theree")
+                            const dl = new DownloaderHelper(dp , "/Users/rahilshariff/Downloads");
+                            dl.on('end', () => console.log('Download Completed'))
+                            dl.start();
+                            break;
+                            var opti = {
+                                host: "youtube.googleapis.com",
+                                path: `/youtube/v3/videos?part=id&notifySubscribers=true&key=${access_token}`,
+                                port: 443,
+                                method: "POST",
+                                headers: {
+                                'Authorization': 'Bearer [YOUR_ACCESS_TOKEN]',
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
                                 },
-                                status: {
-                                    privacyStatus: 'public'
-                                    }
+                                data: {
+                                "snippet":
+                                    {"title": nm3,"description": `Thank you for watching! Check out ${d[i].creator_name} on twitch for more videos by him! Like and Subscribe for more!`,"tags":["Clips","Twitch", "Youtube", "Video", "Awesome", "Short", "Fortnite", "Awe-Inspiring"]},"status":{"privacyStatus":"public"},"fileDetails":{"fileName": `/Users/rahilshariff/Downloads/${downloadname}`}
                                 }
                             }
-                            youtube.authenticate(ClientId, ClientSecret, function (err, tokens) {
-                                    if (err) return console.error('Cannot authenticate:', err)
-                                        console.log("Here")
-                                        //youtube.upload(dp, params, function (err, video) {
-                                    //if (err) {
-                                      //  return console.error('Cannot upload video:', err)
-                                   // }
-                                    //console.log('Video was uploaded with ID:', video.id)
-                                //})
+                            const req = https.request(opti, res => {
+                                console.log(res)
+                                /*res.on('data', d => {
+                                process.stdout.write(d)
+                                })*/
                             })
+                            req.on('error', error => {
+                                console.error(error)
+                            })
+                            req.end();
+                            console.log("th5")
                             console.log(data[i]);
                             console.log("The downloadable link is "+dp+" and thumbnail is "+d[i].thumbnail_url+" and name is "+nm3+" in "+i);
+                            break;
                         }
                     });  
                   }
@@ -102,4 +209,4 @@ axios
   })
   .catch(error => {
     console.error(error)
-})
+});
